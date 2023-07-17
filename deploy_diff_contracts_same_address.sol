479b4da1d315b147b3741a42078ac94e1a066da8
lcadafalch <56115195+lcadafalch@users.noreply.github.com>
2023-07-17 11:01:16 +0200

    Update deploy_diff_contracts_same_address.sol

--- deploy_diff_contracts_same_address.sol
+++ deploy_diff_contracts_same_address.sol
@@ -1,3 +1,14 @@
+
+Contract address deployed with create is computed in the following way.
+
+
+contract address = last 20 bytes of sha3(rlp_encode(sender, nonce))
+where sender is the address of the deployer and nonce is the number of transactions sent by sender.
+
+Hence it is possible to deploy different contracts at the same address if we can somehow reset the nonce.
+
+Below is an example of how a DAO can be hacked.
+
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.17;
 
