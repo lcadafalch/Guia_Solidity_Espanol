c1c586a2c15e12eb90097a1bf46e46f1ca3e4310
lcadafalch <56115195+lcadafalch@users.noreply.github.com>
2023-08-10 11:10:23 +0200

    Update DelegateCall.sol

--- DelegateCall.sol
+++ DelegateCall.sol
@@ -10,27 +10,30 @@
 pragma solidity ^0.8.17;
 
 /*
-HackMe is a contract that uses delegatecall to execute code.
-It is not obvious that the owner of HackMe can be changed since there is no
-function inside HackMe to do so. However an attacker can hijack the
-contract by exploiting delegatecall. Let's see how.
+HackMe es un contrato que utiliza la llamada delegada para ejecutar código.
+No es obvio que se pueda cambiar el propietario de HackMe ya que no hay
+dentro de HackMe para hacerlo. Sin embargo, un atacante puede secuestrar el
+contrato mediante la explotación de la llamada delegada,
+veamos como.
 
-1. Alice deploys Lib
-2. Alice deploys HackMe with address of Lib
-3. Eve deploys Attack with address of HackMe
+
+1. Alice despliega Lib
+2. Alice despkiega HackMe con la direccion of Lib
+3. Eve despliega Attack con la direccion de HackMe
 4. Eve calls Attack.attack()
-5. Attack is now the owner of HackMe
+5. Attack es ahora dueñp de HackMe
 
-What happened?
-Eve called Attack.attack().
-Attack called the fallback function of HackMe sending the function
-selector of pwn(). HackMe forwards the call to Lib using delegatecall.
-Here msg.data contains the function selector of pwn().
-This tells Solidity to call the function pwn() inside Lib.
-The function pwn() updates the owner to msg.sender.
-Delegatecall runs the code of Lib using the context of HackMe.
-Therefore HackMe's storage was updated to msg.sender where msg.sender is the
-caller of HackMe, in this case Attack.
+Que ha pasado?
+Eve ha llamado a  Attack.attack().
+Attack ejecuta la funcion fallback de HackMe a través dek selector pwn().
+HackMe  continua la llamada hacia Lib usando delegatecall.
+Aqui msg.data contiene la funcion selector de pwn().
+Aqui dice a Solidity de ejecutar la funcion pwn() dentro de Lib.
+La funcion pwn() actualiza el propietario del msg.sender.
+
+Delegatecall ejecuta el codigo de Lib usando el contexto de Hackme.
+Por lo tanto el storage de Hackme se actualiza a msg.sender y msg.sender es el propietario de Hackme , por lo tanto el atacante.
+.
 */
 
 contract Lib {
