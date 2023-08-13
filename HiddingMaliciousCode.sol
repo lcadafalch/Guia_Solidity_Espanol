e44131c14d31530821de3ec89240f8908379711a
lcadafalch <56115195+lcadafalch@users.noreply.github.com>
2023-08-13 05:07:16 +0200

    Update HiddingMaliciousCode.sol

--- HiddingMaliciousCode.sol
+++ HiddingMaliciousCode.sol
@@ -1,19 +1,26 @@
+ // Escondiendo código malicioso en un contrato externo // Hiding Malicious Code with External Contract
+
+// VULNERABILIDAD
+/*
+En Solidity, cualquier dirección se puede convertir en un contrato específico, incluso si el contrato en la dirección no es el que se está emitiendo.
+Esto puede explotarse para ocultar código malicioso. Veamos cómo
+*/
+
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.17;
 
 /*
-Let's say Alice can see the code of Foo and Bar but not Mal.
-It is obvious to Alice that Foo.callBar() executes the code inside Bar.log().
-However Eve deploys Foo with the address of Mal, so that calling Foo.callBar()
-will actually execute the code at Mal.
+Digamos que Alice puede ver el código de Foo y Bar pero no Mal.
+Es obvio para Alice que Foo.callBar() ejecuta el código dentro de Bar.log().
+Sin embargo, Eve implementa a Foo con la dirección de Mal, por lo que llamar a Foo.callBar()
+en realidad ejecutará el código en Mal.
 */
 
 /*
-1. Eve deploys Mal
-2. Eve deploys Foo with the address of Mal
-3. Alice calls Foo.callBar() after reading the code and judging that it is
-   safe to call.
-4. Although Alice expected Bar.log() to be execute, Mal.log() was executed.
+1. Eve despliega Mal
+2. Eve despliega Foo con la dirección de Mal
+3. Alice llama a Foo.callBar() después de leer el código y puens que es seguro para llamar.
+4. Aunque Alice esperaba que se ejecutara Bar.log(), se ejecutará Mal.log().
 */
 
 contract Foo {
@@ -35,18 +42,26 @@ contract Bar {
         emit Log("Bar was called");
     }
 }
-
-// This code is hidden in a separate file
+// Este código está oculto en un archivo separado
 contract Mal {
     event Log(string message);
 
-    // function () external {
-    //     emit Log("Mal was called");
-    // }
-
-    // Actually we can execute the same exploit even if this function does
-    // not exist by using the fallback
+// function () external {
+//     emit Log("Mal was called");
+// }
+// En realidad, podemos ejecutar el mismo exploit incluso si esta función lo hace
+// no existe usando la funcion 
     function log() public {
         emit Log("Mal was called");
     }
+}
+
+// TÉCNICAS PREVENTIVAS
+// Inicializar un nuevo contrato dentro del constructor.
+// Hacer pública la dirección del contrato externo para que se pueda revisar el código del contrato externo
+
+Bar public bar;
+
+constructor() public {
+    bar = new Bar();
 }
