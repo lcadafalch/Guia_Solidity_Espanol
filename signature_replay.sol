0405cf9c2e88fe619d459a99fb7f73c937551d5b
lcadafalch <56115195+lcadafalch@users.noreply.github.com>
2023-09-02 12:06:49 +0200

    Update signature_replay.sol

--- signature_replay.sol
+++ signature_replay.sol
@@ -1,47 +1,10 @@
+# Reproducción de Signatura // Signature Replay
 
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.17;
+Firmar mensajes fuera de la cadena y tener un contrato que requiera esa firma antes de ejecutar una función es una técnica útil.
 
-import "github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v4.5/contracts/utils/cryptography/ECDSA.sol";
+Por ejemplo, esta técnica se utiliza para:
 
-contract MultiSigWallet {
-    using ECDSA for bytes32;
+reducir el número de transacciones en la blockchain
+Hacer transacciones sin gas, llamadas
 
-    address[2] public owners;
-
-    constructor(address[2] memory _owners) payable {
-        owners = _owners;
-    }
-
-    function deposit() external payable {}
-
-    function transfer(address _to, uint _amount, bytes[2] memory _sigs) external {
-        bytes32 txHash = getTxHash(_to, _amount);
-        require(_checkSigs(_sigs, txHash), "invalid sig");
-
-        (bool sent, ) = _to.call{value: _amount}("");
-        require(sent, "Failed to send Ether");
-    }
-
-    function getTxHash(address _to, uint _amount) public view returns (bytes32) {
-        return keccak256(abi.encodePacked(_to, _amount));
-    }
-
-    function _checkSigs(
-        bytes[2] memory _sigs,
-        bytes32 _txHash
-    ) private view returns (bool) {
-        bytes32 ethSignedHash = _txHash.toEthSignedMessageHash();
-
-        for (uint i = 0; i < _sigs.length; i++) {
-            address signer = ethSignedHash.recover(_sigs[i]);
-            bool valid = signer == owners[i];
-
-            if (!valid) {
-                return false;
-            }
-        }
-
-        return true;
-    }
-}
+### Vulnerabilidad
