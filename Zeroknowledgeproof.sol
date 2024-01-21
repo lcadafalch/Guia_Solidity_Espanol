8550fcec1eab4509808821e53f95e728d6296c92
lcadafalch <56115195+lcadafalch@users.noreply.github.com>
2024-01-21 11:18:15 +0100

    Update Zeroknowledgeproof.sol

--- Zeroknowledgeproof.sol
+++ Zeroknowledgeproof.sol
@@ -1,2 +1,4 @@
 //TODO AÑADIR ZERO KNOWLEDGE PROOF
 //Zksnark Circom
+https://linea.build/?gad_source=1&gclid=Cj0KCQiAnrOtBhDIARIsAFsSe53BNOE5YwzeaRSFr4J7c9wXyrwjZMDCGXabpYIUF0I9cTnDm8vDM2gaArkuEALw_wcB
+
